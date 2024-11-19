8f8eae4e55bf48c2b8e417181c25b66edf084770
Vachi <hillz246@gmail.com>
2024-11-19 09:06:52 -0500

    Add files via upload
    Added SQL schema

---
+++ schema.sql
@@ -0,0 +1,72 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+--ppnote: for float data- for (decimal) we would use numeric in sql/quickDBD; first find out primary key, for foreign key (docs - relation pick any from there), varchar () in () we can put extra number, for char we have to put exact number of lenght, integer only numbers no decimal point; for foreign key FK then relation >- then classname.column name e.g. FK >- contacts.contact_id; to import table need to have in correct order input ACID (ACID stands for Atomicity, Consistency, Isolation, and Durability. ); find out length of character/varchar - in excel =LEN(B2) then use double click/drag and get length of whole column, then =MAX(B:B) get max number of column B to get highest length of column
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(40)   NOT NULL,
+    "description" varchar(60)   NOT NULL,
+    "goal" numeric   NOT NULL,
+    "pledged" numeric   NOT NULL,
+    "outcome" varchar(20)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" char(5)   NOT NULL,
+    "currency" char(5)   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" char(10)   NOT NULL,
+    "subcategory_id" varchar(8)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" char(4)   NOT NULL,
+    "category" varchar(15)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(15)   NOT NULL,
+    "last_name" varchar(15)   NOT NULL,
+    "email" varchar(45)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" char(8)   NOT NULL,
+    "subcategory" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+SELECT *
+FROM contacts;
+
+SELECT *
+FROM category;
+
+SELECT *
+FROM subcategory;
+
+SELECT *
+FROM campaign;
+
